1b4b16dbdf572cbb59c4137e16de269f8dd932ef
siike92 <siike92@hotmail.com>
2020-08-18 01:06:56 -0500

    Progress on SH1106 OLED

---
+++ include/OLED_SH1106.hpp
@@ -0,0 +1,62 @@
+#ifndef OLED_SH1106_HPP
+#define OLED_SH1106_HPP
+
+#include "LLPD.hpp"
+
+#define SH1106_LCDBUFFERWIDTH 		132
+#define SH1106_LCDWIDTH 		128
+#define SH1106_LCDHEIGHT 		64
+#define SH1106_NUMPAGES 		8
+#define SH1106_SETCONTRAST 		0x81
+#define SH1106_SETNORMALDISPLAY 	0xA6
+#define SH1106_SETREVERSEDISPLAY 	0xA7
+#define SH1106_SETDISPLAYOFF 		0xAE
+#define SH1106_SETDISPLAYON 		0xAF
+#define SH1106_SETCHARGEPUMP6_4V 	0x30
+#define SH1106_SETCHARGEPUMP7_4V 	0x31
+#define SH1106_SETCHARGEPUMP8V 		0x32
+#define SH1106_SETCHARGEPUMP9V 		0x33
+#define SH1106_SETDISPLAYOFFSET 	0xD3
+#define SH1106_SETCOMPINS 		0xDA
+#define SH1106_SETVCOMDETECT 		0xDB
+#define SH1106_SETDISPLAYCLOCKDIV 	0xD5
+#define SH1106_SETPRECHARGE 		0xD9
+#define SH1106_SETMULTIPLEX 		0xA8
+#define SH1106_SETSEGREMAPRIGHT 	0xA1
+#define SH1106_SETSEGREMAPLEFT 		0xA0
+#define SH1106_SETLOWCOLUMN 		0x00
+#define SH1106_SETHIGHCOLUMN 		0x10
+#define SH1106_SETPAGEADDRESS           0xB0
+#define SH1106_SETSTARTLINE 		0x40
+#define SH1106_SETCOMSCANINC 		0xC0
+#define SH1106_SETCOMSCANDEC 		0xC8
+
+class Oled_SH1106
+{
+	public:
+		Oled_SH1106 (const SPI_NUM& spiNum, const GPIO_PORT& csPort, const GPIO_PIN& csPin, const GPIO_PORT& dcPort,
+				const GPIO_PIN& dcPin, const GPIO_PORT& rstPort, const GPIO_PIN& rstPin);
+		~Oled_SH1106();
+
+		void begin();
+
+		void displayFullBuffer (uint8_t* buffer);
+
+		void reset();
+
+		void test();
+
+	private:
+		SPI_NUM   m_SpiNum;
+		GPIO_PORT m_CSPort; // chip select pin port
+		GPIO_PIN  m_CSPin;
+		GPIO_PORT m_DCPort; // data/command pin port
+		GPIO_PIN  m_DCPin;
+		GPIO_PORT m_RstPort; // reset pin port
+		GPIO_PIN  m_RstPin;
+
+		void sendCommand (uint8_t command);
+		void sendData (uint8_t data);
+};
+
+#endif // OLED_SH1106_HPP
---
+++ src/OLED_SH1106.cpp
@@ -0,0 +1,211 @@
+#include "OLED_SH1106.hpp"
+
+Oled_SH1106::Oled_SH1106 (const SPI_NUM& spiNum, const GPIO_PORT& csPort, const GPIO_PIN& csPin, const GPIO_PORT& dcPort,
+				const GPIO_PIN& dcPin, const GPIO_PORT& rstPort, const GPIO_PIN& rstPin) :
+	m_SpiNum( spiNum ),
+	m_CSPort( csPort ),
+	m_CSPin( csPin ),
+	m_DCPort( dcPort ),
+	m_DCPin( dcPin ),
+	m_RstPort( rstPort ),
+	m_RstPin( rstPin )
+{
+}
+
+Oled_SH1106::~Oled_SH1106()
+{
+}
+
+void Oled_SH1106::begin()
+{
+	this->reset();
+
+	// the display needs to be off for certain commands to be issued
+	this->sendCommand( SH1106_SETDISPLAYOFF );
+
+	// set the clock prescaler
+	this->sendCommand( SH1106_SETDISPLAYCLOCKDIV );
+	this->sendCommand( 0x80 ); // TODO this may need to be tweaked
+
+	// set multiplex ration mode
+	this->sendCommand( SH1106_SETMULTIPLEX );
+	this->sendCommand( 0x3F ); // TODO this may need to be tweaked
+
+	// set diplay offset
+	this->sendCommand( SH1106_SETDISPLAYOFFSET );
+	this->sendCommand( 0x00 ); // shouldn't need an offset
+
+	// set chargepump voltage
+	this->sendCommand( SH1106_SETCHARGEPUMP6_4V );
+
+	// set the segment direction
+	this->sendCommand( SH1106_SETSEGREMAPRIGHT );
+
+	// set the com scan direction
+	this->sendCommand( SH1106_SETCOMSCANDEC );
+
+	// set the common pads hardware configuration to alternative (0x02 would be sequential)
+	this->sendCommand( SH1106_SETCOMPINS );
+	this->sendCommand( 0x12 );
+
+	// set the contrast
+	this->sendCommand( SH1106_SETCONTRAST );
+	this->sendCommand( 0xCF ); // TODO this may need to be tweaked
+
+	// set the charge/discharge periods for the charge pump
+	this->sendCommand( SH1106_SETPRECHARGE );
+	this->sendCommand( 0xF1 ); // TODO this may need to be tweaked
+
+	// set the common pad output voltage level at the deselect stage
+	this->sendCommand( SH1106_SETVCOMDETECT );
+	this->sendCommand( 0x40 );
+
+	// set the display layout to normal
+	this->sendCommand( SH1106_SETNORMALDISPLAY );
+
+	// set the RAM start line (this can be used for scrolling, but for now we'll leave it here)
+	this->sendCommand( SH1106_SETSTARTLINE );
+
+	// turn the display back on
+	this->sendCommand( SH1106_SETDISPLAYON );
+}
+
+void Oled_SH1106::displayFullBuffer (uint8_t* buffer)
+{
+	for ( unsigned int page = 0; page < SH1106_NUMPAGES; page++ )
+	{
+		this->sendCommand( SH1106_SETPAGEADDRESS | page );
+		this->sendCommand( SH1106_SETLOWCOLUMN | 0x2 ); // since we aren't using scrolling,
+								// the first and last 2 columns don't matter
+		this->sendCommand( SH1106_SETHIGHCOLUMN | 0x0 );
+		this->sendCommand( SH1106_SETSTARTLINE | 0x0 );
+
+		unsigned int pageOffset = page * SH1106_LCDWIDTH;
+
+		for ( unsigned int column = 0; column < SH1106_LCDWIDTH; column++ )
+		{
+			uint8_t colAdj = column % 8;
+			unsigned int rowOffset = SH1106_LCDWIDTH / 8;
+
+			uint8_t newByte = 0x0;
+			newByte |= (( buffer[(column / 8) + pageOffset + (rowOffset * 0)] & (1 << colAdj) ) >> colAdj << 0);
+			newByte |= (( buffer[(column / 8) + pageOffset + (rowOffset * 1)] & (1 << colAdj) ) >> colAdj << 1);
+			newByte |= (( buffer[(column / 8) + pageOffset + (rowOffset * 2)] & (1 << colAdj) ) >> colAdj << 2);
+			newByte |= (( buffer[(column / 8) + pageOffset + (rowOffset * 3)] & (1 << colAdj) ) >> colAdj << 3);
+			newByte |= (( buffer[(column / 8) + pageOffset + (rowOffset * 4)] & (1 << colAdj) ) >> colAdj << 4);
+			newByte |= (( buffer[(column / 8) + pageOffset + (rowOffset * 5)] & (1 << colAdj) ) >> colAdj << 5);
+			newByte |= (( buffer[(column / 8) + pageOffset + (rowOffset * 6)] & (1 << colAdj) ) >> colAdj << 6);
+			newByte |= (( buffer[(column / 8) + pageOffset + (rowOffset * 7)] & (1 << colAdj) ) >> colAdj << 7);
+
+			this->sendData( newByte );
+		}
+	}
+}
+
+void Oled_SH1106::reset()
+{
+	LLPD::gpio_output_set( m_RstPort, m_RstPin, true );
+
+	// delay to ensure reset
+	LLPD::tim6_delay( 10000 );
+
+	LLPD::gpio_output_set( m_RstPort, m_RstPin, false );
+
+	// delay to ensure reset
+	LLPD::tim6_delay( 10000 );
+
+	LLPD::gpio_output_set( m_RstPort, m_RstPin, true );
+
+	// delay to ensure reset
+	LLPD::tim6_delay( 10000 );
+}
+
+void Oled_SH1106::test()
+{
+	/*
+	// reset code
+	LLPD::gpio_output_set( m_RstPort, m_RstPin, true );
+	// delay to ensure reset
+	LLPD::tim6_delay( 10000 );
+	LLPD::gpio_output_set( m_RstPort, m_RstPin, false );
+	// delay to ensure reset
+	LLPD::tim6_delay( 10000 );
+	LLPD::gpio_output_set( m_RstPort, m_RstPin, true );
+	// delay to ensure reset
+	LLPD::tim6_delay( 10000 );
+
+	// begin code
+	this->sendCommand( SH1106_DISPLAYOFF );
+	this->sendCommand( SH1106_SETDISPLAYCLOCKDIV );
+	this->sendCommand( 0x80 ); // TODO this may need to be tweaked
+	this->sendCommand( SH1106_SETMULTIPLEX );
+	this->sendCommand( 0x3F ); // TODO this may need to be tweaked
+	this->sendCommand( SH1106_SETDISPLAYOFFSET );
+	this->sendCommand( 0x00 ); // shouldn't need an offset
+	this->sendCommand( SH1106_SETSTARTLINE );
+	this->sendCommand( SH1106_CHARGEPUMP );
+	// TODO or this??? this->sendCommand( 0x10 );
+	this->sendCommand( 0x14 );
+	this->sendCommand( SH1106_MEMORYMODE );
+	this->sendCommand( 0x00 );
+	this->sendCommand( SH1106_SEGREMAP | 0x1 );
+	this->sendCommand( SH1106_COMSCANDEC );
+	this->sendCommand( SH1106_SETCOMPINS );
+	this->sendCommand( 0x12 );
+	this->sendCommand( SH1106_SETCONTRAST );
+	// TODO or this??? this->sendCommand( 0x9F );
+	this->sendCommand( 0xCF );
+	this->sendCommand( SH1106_SETPRECHARGE );
+	// TODO or this??? this->sendCommand( 0x22 );
+	this->sendCommand( 0xF1 );
+	this->sendCommand( SH1106_SETVCOMDETECT );
+	this->sendCommand( 0x40 );
+	this->sendCommand( SH1106_DISPLAYALLON_RESUME );
+	this->sendCommand( SH1106_NORMALDISPLAY );
+
+	this->sendCommand( SH1106_DISPLAYON );
+	*/
+
+	// draw a single pixel
+	this->sendCommand( SH1106_SETLOWCOLUMN | 0x0 );
+	this->sendCommand( SH1106_SETHIGHCOLUMN | 0x0 );
+	this->sendCommand( SH1106_SETSTARTLINE | 0x0 );
+	this->sendData( 0xFF );
+	this->sendData( 0xFF );
+	this->sendData( 0xFF );
+	this->sendData( 0xFF );
+	this->sendData( 0xFF );
+	this->sendData( 0xFF );
+	this->sendData( 0xFF );
+	this->sendData( 0xFF );
+	this->sendData( 0xFF );
+	this->sendData( 0xFF );
+	this->sendData( 0x00 );
+	this->sendData( 0x00 );
+	this->sendData( 0x00 );
+	this->sendData( 0x00 );
+	this->sendData( 0x00 );
+	this->sendData( 0x00 );
+	this->sendData( 0x00 );
+	this->sendData( 0x00 );
+	this->sendData( 0x00 );
+	this->sendData( 0x00 );
+}
+
+void Oled_SH1106::sendCommand (uint8_t command)
+{
+	LLPD::gpio_output_set( m_CSPort, m_CSPin, true );
+	LLPD::gpio_output_set( m_DCPort, m_DCPin, false );
+	LLPD::gpio_output_set( m_CSPort, m_CSPin, false );
+	LLPD::spi_master_send_and_recieve( m_SpiNum, command );
+	LLPD::gpio_output_set( m_CSPort, m_CSPin, true );
+}
+
+void Oled_SH1106::sendData (uint8_t data)
+{
+	LLPD::gpio_output_set( m_CSPort, m_CSPin, true );
+	LLPD::gpio_output_set( m_DCPort, m_DCPin, true );
+	LLPD::gpio_output_set( m_CSPort, m_CSPin, false );
+	LLPD::spi_master_send_and_recieve( m_SpiNum, data );
+	LLPD::gpio_output_set( m_CSPort, m_CSPin, true );
+}
