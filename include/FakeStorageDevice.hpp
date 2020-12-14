f2e943b6d7e935a2fb9ac919e7b618f009259d7e
siike92 <siike92@hotmail.com>
2020-12-14 12:35:32 -0600

    Added a fake storage device to simulate an eeprom, sram, ect

---
+++ include/FakeStorageDevice.hpp
@@ -0,0 +1,31 @@
+#ifndef FAKESTORAGEDEVICE_HPP
+#define FAKESTORAGEDEVICE_HPP
+
+/**************************************************************************
+ * A FakeStorageDevice simulates a physical storage device such as a
+ * eeprom, sram, ect.
+**************************************************************************/
+
+#include "IStorageMedia.hpp"
+
+class FakeStorageDevice : public IStorageMedia
+{
+	public:
+		FakeStorageDevice (const unsigned int deviceSizeInBytes);
+		~FakeStorageDevice() override;
+
+		void writeToMedia (const SharedData<uint8_t>& data, const unsigned int offsetInBytes) override;
+		SharedData<uint8_t> readFromMedia (const unsigned int sizeInBytes, const unsigned int offsetInBytes) override;
+
+		bool needsInitialization() override { return false; }
+		void initialize() override {}
+		void afterInitialize() override {}
+
+		bool hasMBR() override { return false; }
+
+	private:
+		unsigned int 	m_SizeInBytes;
+		uint8_t* 	m_DataArray;
+};
+
+#endif // FAKESTORAGEDEVICE_HPP
---
+++ src/FakeStorageDevice.cpp
@@ -0,0 +1,35 @@
+#include "FakeStorageDevice.hpp"
+
+FakeStorageDevice::FakeStorageDevice (const unsigned int deviceSizeInBytes) :
+	m_SizeInBytes( deviceSizeInBytes ),
+	m_DataArray( new uint8_t[deviceSizeInBytes] )
+{
+}
+
+FakeStorageDevice::~FakeStorageDevice()
+{
+	delete[] m_DataArray;
+}
+
+void FakeStorageDevice::writeToMedia (const SharedData<uint8_t>& data, const unsigned int offsetInBytes)
+{
+	if ( data.getSizeInBytes() + offsetInBytes < m_SizeInBytes ) // if the data fits in this media
+	{
+		for ( unsigned int byte = 0; byte < data.getSizeInBytes(); byte ++ )
+		{
+			m_DataArray[offsetInBytes + byte] = data[byte];
+		}
+	}
+}
+
+SharedData<uint8_t> FakeStorageDevice::readFromMedia (const unsigned int sizeInBytes, const unsigned int offsetInBytes)
+{
+	SharedData<uint8_t> data = SharedData<uint8_t>::MakeSharedData( sizeInBytes );
+
+	for ( unsigned int byte = 0; byte < sizeInBytes; byte++ )
+	{
+		data[byte] = m_DataArray[offsetInBytes + byte];
+	}
+
+	return data;
+}
